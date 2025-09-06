8757f01658273ae6b50433a6dae7a9ae5a3cd903
chandandas-3025 <chandandas.mail.for.official.work@gmail.com>
2025-09-06 13:33:28 +0530

    Add fish config file for future

---
+++ fish/config.fish
@@ -0,0 +1,5 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    set fish_greeting
+    fastfetch -l ubuntu_old2_small -s OS:Host:Kernel:CPU:GPU:Memory:Uptime
+end
